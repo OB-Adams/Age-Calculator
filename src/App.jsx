2a373d17cb18e2528ba71f2e788802026016bf33
OB-Adams <obobadams@gmail.com>
2024-07-26 10:13:17 +0000

    Final commit!!

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="main-wrapper grid place-content-center bg-offWhite w-screen h-screen">
       <AgeCalculator />
-      <p className="mt-4 text-center font-mono text-blue-900">Designed and Coded by <a href="https://github.com/OB-Adams">OB</a></p>
+      <p className="mt-4 text-center font-mono ">Designed and Coded by <a className="text-blue-900" href="https://github.com/OB-Adams">OB</a></p>
     </div>
   )
 }
